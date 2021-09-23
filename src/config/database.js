ab0d9f1f2c952958ccb36de629b801e894ac131c
luisclaudiorp <luis.ramalho@sou.ucpel.edu.br>
2021-09-22 23:54:43 -0300

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: "localhost",
+  username: "root",
+  password: "123456",
+  database: "tdd",
+  dialect: "mysql",
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
